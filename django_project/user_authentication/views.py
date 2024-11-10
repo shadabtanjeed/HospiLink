14021a1a188b0b22c6a452536f3ea5a381533af5
shadabtanjeed <127338881+shadabtanjeed@users.noreply.github.com>
2024-11-11 00:56:38 +0600

    added back button and hashing function

--- django_project/user_authentication/views.py
+++ django_project/user_authentication/views.py
@@ -1,6 +1,9 @@
 # django_project/user_authentication/views.py
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+import hashlib
+
+
 
 def index(request):
     return render(request, "login_page.html")
@@ -22,6 +25,12 @@ def signup_view(request):
         to_time = request.POST.get("to_time")
         security_question = request.POST.get("security_question")
         security_answer = request.POST.get("security_answer")
+
+        hash_object = hashlib.sha512(password.encode('utf-8'))  
+        password = hash_object.hexdigest()
+
+        hash_object = hashlib.sha512(security_answer.encode('utf-8'))
+        security_answer = hash_object.hexdigest()
 
         # Store the data in variables (for demonstration purposes)
         form_data = {
