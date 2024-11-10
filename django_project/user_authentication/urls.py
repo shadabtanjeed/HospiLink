ee4cede4633d5f36f07e2d94b7a34294adc9c524
shadabtanjeed <127338881+shadabtanjeed@users.noreply.github.com>
2024-11-11 00:28:36 +0600

    added security question

--- django_project/user_authentication/urls.py
+++ django_project/user_authentication/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("login/", views.login_view, name="login"),
-    path("signup/", views.signup_view, name="signup"),  # Add this line
+    path("signup/", views.signup_view, name="signup"),
+     path("signup_success/", views.signup_success, name="signup_success"),
 ]
--- django_project/user_authentication/views.py
+++ django_project/user_authentication/views.py
@@ -1,14 +1,54 @@
+# django_project/user_authentication/views.py
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, "login_page.html")
 
-
 def login_view(request):
     return render(request, "login_page.html")
 
-
 def signup_view(request):
+    if request.method == "POST":
+        user_type = request.POST.get("user_type")
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        phone_number = request.POST.get("phone_number")
+        blood_group = request.POST.get("blood_group")
+        complexities = request.POST.get("complexities")
+        specialization = request.POST.get("specialization")
+        available_days = request.POST.getlist("available_days")
+        from_time = request.POST.get("from_time")
+        to_time = request.POST.get("to_time")
+        security_question = request.POST.get("security_question")
+        security_answer = request.POST.get("security_answer")
+
+        # Store the data in variables (for demonstration purposes)
+        form_data = {
+            "user_type": user_type,
+            "username": username,
+            "password": password,
+            "phone_number": phone_number,
+            "blood_group": blood_group,
+            "complexities": complexities,
+            "specialization": specialization,
+            "available_days": available_days,
+            "from_time": from_time,
+            "to_time": to_time,
+            "security_question": security_question,
+            "security_answer": security_answer,
+        }
+
+        # Store the form data in the session
+        request.session['form_data'] = form_data
+
+        # Redirect to a success page
+        return redirect('signup_success')
+
     return render(request, "signup_page.html")
+
+def signup_success(request):
+    # Retrieve the form data from the session
+    form_data = request.session.get('form_data', {})
+    print(form_data)
+    return HttpResponse(f"Signup successful! Data: {form_data}")
