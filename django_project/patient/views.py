cc9ddad6177f2a4b62f94e3ab5176f118d2d9973
shadabtanjeed <127338881+shadabtanjeed@users.noreply.github.com>
2024-11-17 00:07:42 +0600

    navbar added

---
+++ django_project/patient/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.db import connection
+from django.db.utils import DatabaseError
+import hashlib
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+import json
+
+
+def index(request):
+    return render(request, "patient_page.html")
