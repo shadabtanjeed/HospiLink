598fbe465742af061b059ac8e38d24c09a6f28fe
Tahsinul Islam Rupom <127360244+halumgrr@users.noreply.github.com>
2025-03-11 02:23:31 +0600

    previous appointments seeing functionality added

--- django_project/doctor/urls.py
+++ django_project/doctor/urls.py
@@ -4,9 +4,7 @@ from . import views  # Corrected import path
 urlpatterns = [
     path("", views.index, name="index"),
     path("index/", views.index, name="index"),
-    path(
-        "api/upcoming_appointments/",
-        views.fetch_upcoming_appointments,
-        name="fetch_upcoming_appointments",
-    ),
+    path("api/upcoming_appointments/", views.fetch_upcoming_appointments, name="fetch_upcoming_appointments"),
+    path("previous_appointments/", views.previous_appointments, name="previous_appointments"),
+    path("api/previous_appointments/", views.fetch_previous_appointments, name="fetch_previous_appointments"),
 ]
--- django_project/doctor/views.py
+++ django_project/doctor/views.py
@@ -39,3 +39,42 @@ def fetch_upcoming_appointments(request):
 
     response_data = json.dumps(data)
     return HttpResponse(response_data, content_type="application/json")
+
+
+
+def previous_appointments(request):
+    username = request.session.get("doctor_username", "")
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT public.get_name(%s)", [username])
+        name = cursor.fetchone()[0]
+
+    return render(request, 'previous_appointments.html', {"username": username, "name": name})
+
+
+def fetch_previous_appointments(request):
+    username = request.session.get("login_form_data", {}).get("username")
+    user_type = request.session.get("login_form_data", {}).get("user_type")
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT * FROM show_past_appointments(%s, %s)", [username, user_type]
+        )
+        columns = [col[0] for col in cursor.description]
+        data = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+        # Convert date and time objects to strings
+        for item in data:
+            if isinstance(item["appointment_date"], date):
+                item["appointment_date"] = item["appointment_date"].strftime("%Y-%m-%d")
+            if isinstance(item["appointment_time"], time):
+                item["appointment_time"] = item["appointment_time"].strftime("%H:%M:%S")
+
+            patient_username = item["related_user"]
+            cursor.execute("SELECT public.get_name(%s)", [patient_username])
+            patient_name = cursor.fetchone()[0]
+            item["patient_name"] = patient_name
+
+    response_data = json.dumps(data)
+    return HttpResponse(response_data, content_type="application/json")
+
