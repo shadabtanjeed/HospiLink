6a255b5008c2477e498bc0a4f7a666b009ffd6e1
Tahsinul Islam Rupom <tahsinulislam21@iut-dhaka.edu>
2025-04-26 11:00:45 +0600

    Revert "Update settings.py"
    This reverts commit add51687fe79620c24dfec08c5483ee38153fa05.

--- django_project/hospilink_django/settings.py
+++ django_project/hospilink_django/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "10.0.0.15"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
