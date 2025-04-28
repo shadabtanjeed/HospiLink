e8d115d69cacc8f827573d81166b0d97f8c28867
Shadab Tanjeed <127338881+shadabtanjeed@users.noreply.github.com>
2025-04-28 13:20:47 +0600

    Added render.com to allowed hostlist

--- django_project/hospilink_django/settings.py
+++ django_project/hospilink_django/settings.py
@@ -37,7 +37,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# filepath: django_project/hospilink_django/settings.py
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'hospilink.onrender.com']
 
 
 # Application definition
